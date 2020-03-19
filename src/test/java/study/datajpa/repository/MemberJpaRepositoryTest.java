06776cb543dc4a64dd6460c34a78daf2b7774b29
freeefly <freefly3557@gmail.com>
2020-03-19 23:16:34 +0900

    feat : test jpa repositories

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,55 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+
+    @Test
+    public void testMember () {
+        // given
+        Member member = new Member("memberA");
+        // when
+        Member savedMember = memberJpaRepository.save(member);
+        // then
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+        assertEquals(findMember.getId(), member.getId());
+        assertEquals(findMember.getUsername(), member.getUsername());
+    }
+
+    @Test
+    public void basicCRUD () {
+        Member member1 = new Member("memberA");
+        Member member2 = new Member("memberB");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertEquals(member1, findMember1);
+        assertEquals(member2, findMember2);
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertEquals(all.size(), 2);
+
+        long count = memberJpaRepository.count();
+        assertEquals(count, 2);
+
+
+    }
+}
